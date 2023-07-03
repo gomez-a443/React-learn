729ff24afd2347f6aa960c9558493fa647466959
gomez-a443 <114755613+gomez-a443@users.noreply.github.com>
2023-07-03 15:21:43 +0530

    Nav-bar and image styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,14 @@
 import Logo from "../assets/Logo.JPG";
 function Navbar() {
   return (
-    <header>
-      <div className="flex ml-28 mr-28 justify-center">
-        <img src={Logo} height="60px" width="60px"></img>
-        <div className="flex">
-          <div>Home</div>
-          <div>About Us</div>
-          <div>Features</div>
-          <div>Purchase</div>
-          <div>Contact Us</div>
-        </div>
+    <header className="flex bg-gray-200 h-60 justify-evenly items-center">
+      <img src={Logo} className=" h-40 w-40 rounded-full"></img>
+      <div className="flex">
+        <div>Home</div>
+        <div>About Us</div>
+        <div>Features</div>
+        <div>Purchase</div>
+        <div>Contact Us</div>
       </div>
     </header>
   );
