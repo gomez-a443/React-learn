edb9520114356d3b134a647b9e9f788e965141b1
gomez-a443 <114755613+gomez-a443@users.noreply.github.com>
2023-07-03 15:40:27 +0530

    navbar implemented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,12 @@ function Navbar() {
   return (
     <header className="flex bg-gray-200 h-60 justify-evenly items-center">
       <img src={Logo} className=" h-40 w-40 rounded-full"></img>
-      <div className="flex">
-        <div>Home</div>
-        <div>About Us</div>
-        <div>Features</div>
-        <div>Purchase</div>
-        <div>Contact Us</div>
+      <div className="flex bg-gray-300">
+        <div className=" bg-gray-600 text-white p-2 pl-4 pr-4">Home</div>
+        <div className="p-2 pl-4 pr-4">About Us</div>
+        <div className="p-2 pl-4 pr-4">Features</div>
+        <div className="p-2 pl-4 pr-4">Purchase</div>
+        <div className="p-2 pl-4 pr-4">Contact Us</div>
       </div>
     </header>
   );
